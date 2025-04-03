1cb41c3ff8c7900934d50b3658e33fbb1ffe7920
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-04-03 20:53:10 +0700

    Added test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -15,4 +15,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+
+    void findMaxMiddleArray () {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 13, 8, 6, 11, 11, 12};
+        long expected = 13;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
